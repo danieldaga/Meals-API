5d0f9afda115883e181f63f9a80e347cee2950b9
danieldaga <daniel@arhrestauracion.com>
2023-02-22 19:50:32 +0100

    include cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const { response } = require("express");
+
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const PORT = 3000;
 const mealsJSON = require("./meals.json")
+
+    app.use(cors());
 
     app.listen(PORT, () =>{
         console.log(`run port ${PORT}`);
